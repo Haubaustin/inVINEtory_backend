854d411260a37f8202e79d2feff9dfb17ef8e2b0
haubaustin <haubaustin@gmail.com>
2022-05-07 15:02:31 -0700

    Auth

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,2 +1,7 @@
 const router = require('express').Router()
 const controller = require('../controllers/AuthController')
+const middleware = require('../middleware')
+
+router.post('/register', controller.Register)
+router.post('/login', controller.Login)
+router.get("/session", middleware.stripToken, middleware.verifyToken, controller.CheckSession)
